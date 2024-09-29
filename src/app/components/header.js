9e39927b2bd9eb56caf6b9d5d9e14bc78a4fa16c
Anktw <at792226@gmail.com>
2024-09-29 16:43:25 +0530

    adding animation

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -64,9 +64,9 @@ const Header = () => {
 
   return (
     <header className="w-full shadow-md">
-      <div className="container mx-auto flex justify-between items-center p-4">
+        <div className="container mx-auto flex justify-between items-center p-4">
         {/* Logo */}
-        <div className="text-2xl font-bold">
+        <div className="text-2xl font-bold animate-fadeInLeft">
           <Link href="/">Ankit Tiwari</Link>
         </div>
 
@@ -102,39 +102,6 @@ const Header = () => {
           >
             Contact
           </Link>
-
-          {/* Theme Toggle Button */}
-          <div
-            className={` w-16 py-1 rounded-full cursor-pointer transition-colors duration-300 ease-in-out ${
-              isThemeOn
-                ? " border-foreground border-solid border-2"
-                : "border-foreground border-solid border-2"
-            }`}
-            onClick={toggleTheme}
-          >
-            <div
-              className={`w-6 h-6 mx-1 rounded-full bg-foreground shadow-md transform transition-transform duration-300 ease-in-out flex items-center justify-center ${
-                isThemeOn ? "translate-x-7" : ""
-              }`}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className={`h-4 w-4 text-background transition-opacity duration-300 ${
-                  isThemeOn ? "opacity-100" : "opacity-0"
-                }`}
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                />
-              </svg>
-            </div>
-          </div>
         </nav>
 
         {/* Social Icons Section */}
@@ -167,7 +134,7 @@ const Header = () => {
               className={`h-6 w-6 ${isThemeOn ? "fill-black" : "fill-white"}`}
               viewBox="0 0 24 24"
             >
-             <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+              <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
             </svg>
           </Link>
 
@@ -183,15 +150,49 @@ const Header = () => {
               className={`h-6 w-6 ${isThemeOn ? "fill-black" : "fill-white"}`}
               viewBox="0 0 24 24"
             >
-              <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"/>
+              <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
             </svg>
           </Link>
         </div>
-
+        {/* Theme Toggle Button */}
+        <div
+            className={`hidden md:block w-16 py-1 rounded-full cursor-pointer transition-colors duration-300 ease-in-out ${
+              isThemeOn
+                ? " border-foreground border-solid border-2"
+                : "border-foreground border-solid border-2"
+            }`}
+            onClick={toggleTheme}
+          >
+            <div
+              className={`w-6 h-6 mx-1 rounded-full bg-foreground shadow-md transform transition-transform duration-300 ease-in-out flex items-center justify-center ${
+                isThemeOn ? "translate-x-7" : ""
+              }`}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className={`h-4 w-4 text-background transition-opacity duration-300 ${
+                  isThemeOn ? "opacity-100" : "opacity-0"
+                }`}
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                />
+              </svg>
+            </div>
+          </div>
         {/* Burger Menu Button */}
         <button
-          className="relative w-8 h-8 md:hidden focus:outline-none z-50"
-          onClick={toggleMenu}
+          className="relative w-8 h-8 md:hidden z-50"
+          onClick={(e) => {e.preventDefault();
+            toggleMenu();
+          }}
+          aria-label="Menu"
         >
           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-6 h-5">
             <span
@@ -259,7 +260,7 @@ const Header = () => {
           >
             Contact
           </Link>
-          
+
           <div
             className={` w-16 py-1 rounded-full cursor-pointer transition-colors duration-300 ease-in-out ${
               isThemeOn
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { transform } = require('next/dist/build/swc');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -10,6 +12,30 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      keyframes: {
+        fadeInUp: {
+          '0%': { opacity: 0, transform: 'translateY(20px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        fadeInDown:{
+          '0%': {opacity: 0, transform: 'translateY(-20px)'},
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        fadeInLeft:{
+          '0%': {opacity: 0, transform: 'translateX(-20px)'},
+          '100%': { opacity: 1, transform: 'translateX(0)' },
+        },
+        fadeInRight:{
+          '0%': {opacity: 0, transform: 'translateX(20px)'},
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        }
+      },
+      animation: {
+        fadeInUp: 'fadeInUp 0.5s ease-out forwards',
+        fadeInDown: 'fadeInDown 0.5s ease-out forwards',
+        fadeInLeft: 'fadeInLeft 0.5s ease-out forwards',
+        fadeInRight: 'fadeInRight 0.5s ease-out forwards',
       },
     },
   },
