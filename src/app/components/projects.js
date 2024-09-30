7901fd4a6c30ae7409b7dc4c3b7f0433645e7083
Anktw <at792226@gmail.com>
2024-09-30 15:55:52 +0530

    Update projects.js

--- src/app/components/projects.js
+++ src/app/components/projects.js
@@ -61,7 +61,7 @@ export default function Projects() {
         {techList.map((tech) => (
           <button
             key={tech}
-            className={`px-4 py-2 m-2 border-2 border-foreground rounded-full ${
+            className={`px-4 py-2 m-1 md:m-2 lg:m-3 border-2 border-foreground rounded-full ${
               selectedTech === tech
                 ? "bg-foreground text-background"
                 : "border-foreground bg-background text-foreground"
