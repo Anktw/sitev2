5b1a66ba1e0c1799e59251659dcbada63bb47fa8
Anktw <at792226@gmail.com>
2024-10-01 19:21:09 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import HeadingHome from "./components/headinghome.jsx";
 export default function Home() {
   return ( 
     <div>
-      <main className="p-0 md:p-2 m-0 md:m-4 lg:p-4">
+      <main className="p-0 md:p-2 m-1 md:m-4 lg:p-4">
         <HeadingHome text="Projects"/>
         <ProjectsComp/>
         <HeadingHome text="Resume"/>
