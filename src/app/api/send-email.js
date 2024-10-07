a0f61500e0f9658923d67a54b7d455d4b3024db2
Anktw <at792226@gmail.com>
2024-10-07 23:25:20 +0530

    changes in password

--- src/app/api/send-email.js
+++ src/app/api/send-email.js
@@ -6,8 +6,8 @@ export default async function handler(req, res) {
 
     let transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
