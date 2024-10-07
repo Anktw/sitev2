001b539c682dcea1bd0e53a6e8b17ed1ead4db8b
Anktw <at792226@gmail.com>
2024-10-07 23:26:34 +0530

    Update send-email.js

--- src/app/api/send-email.js
+++ src/app/api/send-email.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
 
       res.status(200).json({ success: true });
     } catch (error) {
-      console.error("Error sending email: ", error);
+      console.error("Error sending email: ", error.message);
       res.status(500).json({ success: false });
     }
   } else {
