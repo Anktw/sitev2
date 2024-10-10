18db9726f157d9a39b5a7e1b5bec1ba214e44648
Anktw <at792226@gmail.com>
2024-10-10 12:50:12 +0530

    fixed blogs l;ink to blog

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -113,7 +113,7 @@ export function Footer() {
           </div>
         </div>
         <div className="w-full grid grid-cols-2 md:grid-cols-3 gap-4">
-          {["Portfolio", "Projects", "Resume", "Blogs", "About", "Contact"].map(
+          {["Portfolio", "Projects", "Resume", "Blog", "About", "Contact"].map(
             (link) => (
               <Link
                 key={link}
