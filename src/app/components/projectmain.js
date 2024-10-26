ff71452cdf15d1c0d01279400f526a8024575c78
Anktw <at792226@gmail.com>
2024-10-26 23:34:08 +0530

    Update projectmain.js

--- src/app/components/projectmain.js
+++ src/app/components/projectmain.js
@@ -1,55 +1,35 @@
-"use client";
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { getProjects } from "../lib/projects";
+import LoadingBar from "./loader";
 
-export default function ProjectsMain() {
-  const [projects, setProjects] = useState([]);
+export default async function ProjectsMain() {
+  const projects = await getProjects();
+  
+  console.log('Projects data:', projects);
 
-  const fetchProjects = async (tech = "Recent") => {
-    try {
-      const response = await fetch("/projects.json");
-      if (response.ok) {
-        const data = await response.json();
-
-        if (tech === "Recent") {
-          const sortedProjects = data.sort((a, b) => b.id - a.id).slice(0, 3);
-          setProjects(sortedProjects);
-        } else {
-          const filteredProjects = data.filter((project) =>
-            project.techStack.includes(tech)
-          );
-          setProjects(filteredProjects);
-        }
-      } else {
-        console.error("Failed to load projects.");
-      }
-    } catch (error) {
-      console.error("Error fetching projects:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchProjects();
-  }, []);
+  if (!projects || projects.length === 0) {
+    return <LoadingBar/>;
+  }
 
   return (
-    <div className="px-0 md:px-5 lg:px-8 mt-2 md:mt-6 lg:mt-10">
-      {/* Projects Container */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-5 lg:gap-6 animate-fadeInLeft">
+    <div className="px-0 md:px-5 lg:px-8 mt-2 md:mt-6 lg:mt-10 animate-fadeInUp">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-6 lg:gap-8 animate-fadeInLeft">
         {projects.map((project) => (
-          <div key={project.id} className="p-3 rounded-md cursor-pointer">
-            <div className="relative w-full" style={{ paddingTop: "56.25%" }}>
+          <div
+            key={project.id}
+            className="p-1 rounded-md cursor-pointer transition-transform duration-500 transform hover:scale-100 md:hover:scale-100 lg:hover:scale-105"
+          >
+            <div className="aspect-w-16 aspect-h-9">
               <Image
-                className="absolute top-0 left-0 w-full h-full object-cover rounded-xl"
+                className="w-full h-full object-cover rounded-xl"
                 src={project.image}
                 alt={project.title}
-                width={800}
-                height={450}
-                loading="lazy"
+                width={1600}
+                height={900}
+                priority={true}
               />
             </div>
-
-            <div className="p-3">
+            <div className="p-4">
               <h3 className="text-lg font-bold">{project.title}</h3>
               <p className="text-sm mt-2">{project.description}</p>
             </div>
