e612f0f5d935cc12ac2eb45ef1756f42c0ecdbf3
Anktw <at792226@gmail.com>
2024-10-26 21:15:58 +0530

    fixing build error

--- src/app/resume/page.js
+++ src/app/resume/page.js
@@ -1,13 +1,12 @@
 import HeadingMain from "../components/headings/headingmain";
 import ResumeComp from "../components/resume";
-import ResumeComponent from "../components/resumeframe";
 
 export default function Resume() {
   return (
     <main className="px-0 md:px-5 lg:px-8">
       <HeadingMain text="Resume"/>
       <div className="bg-background min-h-screen">
-      <ResumeComponent/>
+      <ResumeComp/>
       </div>
     </main>
   );
