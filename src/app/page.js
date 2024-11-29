a8f81da2ba0578b5fa984b0dd4a702c64ece01eb
Anktw <at792226@gmail.com>
2024-11-30 01:00:49 +0530

    fixed json content

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,12 @@ import GoToBtnHome from "./components/gotobtnshome";
 import BlogsMain from "./components/blogmain";
 import TopBtnsHome from "./components/hometopbtn";
 import AboutMain from "./components/aboutmain";
+import { DiscontinuationNotice } from "./components/discontinue-notice";
 export default function Home() {
   return (
     <div>
       <main className="px-2 md:px-3 lg:px-5 m-1 md:m-4 ">
+        <DiscontinuationNotice/>
         <TopBtnsHome/>
         <HeadingHome text="Recent Projects"/>
         <ProjectsMain />
