c6e7cd694d7f53219062e077e407a61eb23415b3
Ankit Tiwari <140378286+Anktw@users.noreply.github.com>
2025-05-02 05:25:23 +0530

    added redirect url in signup and login

--- src/app/components/ui/buttons/loginbtn.js
+++ src/app/components/ui/buttons/loginbtn.js
@@ -1,23 +1,34 @@
+"use client"
+import { useEffect, useState } from "react"
 import Button1 from "./button1"
-export default function LogInBtn(){
-    return(
-        <div className="inline">
-        <Button1
-          text="Login"
-          href="https://accounts.unkit.site/auth/user/login"
-          icon={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24px"
-              viewBox="0 -960 960 960"
-              width="24px"
-              fill="currentColor"
-              className={`transform transition-transform duration-300 group-hover:scale-110 group-hover:-rotate-45`}
-            >
-              <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
-            </svg>
-          }
-        />
-      </div>
-    )
+
+export default function LogInBtn() {
+  const [loginHref, setLoginHref] = useState("https://accounts.unkit.site/auth/user/login")
+
+  useEffect(() => {
+    const currentUrl = window.location.href
+    const encodedRedirect = encodeURIComponent(currentUrl)
+    setLoginHref(`https://accounts.unkit.site/auth/user/login?redirect=${encodedRedirect}`)
+  }, [])
+
+  return (
+    <div className="inline">
+      <Button1
+        text="Login"
+        href={loginHref}
+        icon={
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="currentColor"
+            className={`transform transition-transform duration-300 group-hover:scale-110 group-hover:-rotate-45`}
+          >
+            <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
+          </svg>
+        }
+      />
+    </div>
+  )
 }
--- src/app/components/ui/buttons/signupbtn.js
+++ src/app/components/ui/buttons/signupbtn.js
@@ -1,12 +1,23 @@
+"use client"
+import { useEffect, useState } from "react"
 import Button1 from "./button1"
-export default function SignUpBtn(){
-    return(
-        <div className="inline">
-        <Button1
-          text="Sign Up"
-          href="https://accounts.unkit.site/auth/user/signup"
-          icon={
-            <svg
+
+export default function SignUpBtn() {
+  const [signupHref, setSignupHref] = useState("https://accounts.unkit.site/auth/user/signup")
+
+  useEffect(() => {
+    const currentUrl = window.location.href
+    const encodedRedirect = encodeURIComponent(currentUrl)
+    setSignupHref(`https://accounts.unkit.site/auth/user/signup?redirect=${encodedRedirect}`)
+  }, [])
+
+  return (
+    <div className="inline">
+      <Button1
+        text="SignUp"
+        href={signupHref}
+        icon={
+          <svg
             xmlns="http://www.w3.org/2000/svg"
             height="24px"
             viewBox="0 -960 960 960"
@@ -16,8 +27,8 @@ export default function SignUpBtn(){
           >
             <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
           </svg>
-          }
-        />
-      </div>
-    )
+        }
+      />
+    </div>
+  )
 }
