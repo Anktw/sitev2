d2fdefa355b8ea0f114a2edd9cb5dd65cb961769
Ankit Tiwari <140378286+Anktw@users.noreply.github.com>
2025-05-14 02:46:54 +0530

    added dark backgrounds to projects

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -22,7 +22,22 @@ export function Footer() {
             This site is entirely built using two primary variables:{" "}
             <span className="font-mono opacity-100">#fff</span> (white) and{" "}
             <span className="font-mono opacity-100">#000</span> (black). The design revolves around the manipulation of these two core colors. This was also my first project using Next.js, serving as a learning experience.
+
           </div>
+          <a href="https://github.com/anktw/site" alt="Source" target="_blank"className="group flex items-center justify-center">Source{" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="inline-block h-4 w-4 transform group-hover:translate-x-1 group-hover:-translate-y-1 transition-transform"
+            aria-hidden="true"
+          >
+            <path
+              fillRule="evenodd"
+              d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+              clipRule="evenodd"
+            ></path>
+          </svg></a>
         </div>
         <div className="w-full">
           <div className="leading-7 mt-4">
