784331a51c43a03a7e51a1ef54c1b68556b00be2
Ankit Tiwari <140378286+Anktw@users.noreply.github.com>
2025-05-15 18:41:55 +0530

    Update page.js

--- src/app/projects/[id]/page.js
+++ src/app/projects/[id]/page.js
@@ -1,7 +1,7 @@
 "use client"
 import { useState, useEffect } from "react"
 import Image from "next/image"
-import { useTheme } from "@/context/Themescontext"
+import { useTheme } from "../../context/Themescontext"
 import LoadingBar from "@/components/loader"
 import Button1 from "@/components/ui/buttons/button1"
 
